4a3652041171a3bccffff27a1ccc836ed127c9b8
Thomas Mauran <thomasmauran@yahoo.com>
2025-05-29 14:08:17 +0200

    feat: setup of the pv and pvc
    Signed-off-by: Thomas Mauran <thomasmauran@yahoo.com>

--- terraform/modules/api-deployment/main.tf
+++ terraform/modules/api-deployment/main.tf
@@ -2,11 +2,60 @@ variable "region" {
   type = string
 }
 
+# API namespace to deploy our app
 resource "kubernetes_namespace" "api-namespace" {
   metadata {
     name = "api"
   }
 }
+
+# Persistent volume
+resource "kubernetes_persistent_volume" "api-pv" {
+  metadata {
+    name = "api-pv"
+  }
+
+  spec {
+    capacity = {
+      storage = "1Gi"
+    }
+
+    access_modes = ["ReadWriteOnce"]
+    storage_class_name               = "local-path"
+
+    persistent_volume_reclaim_policy = "Retain"
+
+    persistent_volume_source {
+      host_path {
+        path = "/mnt/data"
+        type = "Directory"
+      }
+    }
+  }
+}
+
+# Persistent volume claim
+resource "kubernetes_persistent_volume_claim" "api-pvc" {
+  metadata {
+    name      = "api-pvc"
+    namespace = kubernetes_namespace.api-namespace.metadata[0].name
+  }
+
+  spec {
+    access_modes = ["ReadWriteOnce"]
+
+    resources {
+      requests = {
+        storage = "1Gi"
+      }
+    }
+
+    storage_class_name = "local-path"
+    volume_name        = kubernetes_persistent_volume.api-pv.metadata[0].name
+  }
+}
+
+
 
 resource "helm_release" "api" {
   name       = "api"
